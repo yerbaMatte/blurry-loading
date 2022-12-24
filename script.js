d2c8825dd019ac45506de32bba0bdca83d3fcefb
Milosz Lewandowski <milosz.lewandowski@icloud.com>
2022-12-24 17:11:35 +0100

    blurry loading

--- script.js
+++ script.js
@@ -13,4 +13,10 @@ function blurring() {
   }
 
   loadText.innerHTML = `${load}%`;
+  loadText.style.opacity = scale(load, 0, 100, 0, 1);
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
 }
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
